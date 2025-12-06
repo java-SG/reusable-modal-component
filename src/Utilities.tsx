00445753e695f62912e88156fa45519bd2464628
java-SG <carlo_van_der_sloot@hotmail.com>
2025-12-06 14:18:41 +0100

    Forgot interactive styling on close button

--- src/Utilities.tsx
+++ src/Utilities.tsx
@@ -8,11 +8,11 @@ export function CloseButton({ onClick }: { onClick: () => void }) {
     return (
         <div className="absolute top-5 right-5">
             <button
-                className="relative h-10 aspect-square flex justify-center items-center bg-white rounded-full border-black border-2"
+                className="relative h-10 aspect-square flex justify-center items-center bg-white [&>*]:bg-black rounded-full border-black border-2 hover:bg-black hover:[&>*]:bg-white transition duration-300 cursor-pointer"
                 onClick={onClick}
                 aria-label="Close">
-                <span className="absolute w-2/3 h-[2.5px] rotate-45 bg-black rounded" />
-                <span className="absolute w-2/3 h-[2.5px] -rotate-45 bg-black rounded" />
+                <span className="absolute w-2/3 h-[2.5px] rotate-45 rounded" />
+                <span className="absolute w-2/3 h-[2.5px] -rotate-45 rounded" />
             </button>
         </div>
     )
